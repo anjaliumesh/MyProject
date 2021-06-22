bb8c732adb18a4b46bc4e28400c619641141251d
anjali.ku <anjali.ku@inbl01lap1312.brd.g-c-i.com>
2021-06-22 13:10:01 +0530

    new commit

---
+++ java-project/src/test/java/com/skillenza/qa/AppTest.java
@@ -0,0 +1,28 @@
+package com.skillenza.qa;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class AppTest 
+{
+	//System.setProperty("webdriver.chrome.driver","C:\Users\anjali.ku\Desktop\Drivers\\chromedriver.exe");
+	
+	WebDriver driver=new ChromeDriver();
+	
+	
+	
+
+
+
+
+
+
+
+
+
+
+}
